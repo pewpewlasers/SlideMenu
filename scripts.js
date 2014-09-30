6f728e963b5c6a7d7d02f78b052d1af459753a70
Garrett Olson <garrettdolson@gmail.com>
2014-09-29 21:39:36 -0700

    Add subnav menu and jquery to control it

--- scripts.js
+++ scripts.js
@@ -1,39 +1,47 @@
+var navExpanded,
+    initialLoadPage,
+    currentlySelectedLink;
+
 $(document).ready(function() {
 	navExpanded = true;
-	var initialLoadPage = true;
-	var currentLinkName;
-    updateNavMenuState();
+	initialLoadPage = true;
+
+    updateDebugInfo();
 
     $('.nav-link').on('click', function() {
-    	if($(this).attr('name') !== currentLinkName) {
-  /*  		currentLinkName = $(this).attr('name');
-	    	navExpanded = !navExpanded;
-	    	$('#push-panel').toggleClass('collapsed');*/
-	    	initialLoadPage = false;
-            updateNavMenuState(); 	   	
-    	}
+	    	initialLoadPage = false;            
+
+            if($(this).attr('name') !== currentlySelectedLink) {
+                var activeSubNav = $('.treatment-subnav.active');
+                activeSubNav.removeClass('active');
+            }
+
+            currentlySelectedLink = $(this).attr('name');
+
+            updateDebugInfo();
     });
     $('#nav-menu').on('mouseenter', function() {
      	if(!navExpanded) {     		
     		$('#push-panel').toggleClass('collapsed');
     		navExpanded = !navExpanded;
-            updateNavMenuState()
+            updateDebugInfo()
      	}    	
-    });
+    });    
     $('#nav-menu').on('mouseleave', function() {
      	if(navExpanded && !initialLoadPage) {   	
     		$('#push-panel').toggleClass('collapsed');
     		navExpanded = !navExpanded;   
-            updateNavMenuState()	
+            updateDebugInfo()	
      	}    	
     });
     $('#nav-menu').on('click', "a[name='treatment']", function() {    
             $('.treatment-subnav').addClass('active');
-            updateNavMenuState()    
+            updateDebugInfo()    
     });
 });
 
-function updateNavMenuState() {
-    $('.nav-menu-state').html('Nav <br>open?<br>' + navExpanded.toString().toUpperCase());
+function updateDebugInfo() {
+    $('.nav-menu-state').html('Nav-Menu Open? ' + navExpanded.toString().toUpperCase());
+    $('.currently-selected-link').html('Current Page: ' + currentlySelectedLink);
 }
 
