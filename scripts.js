3c4c0e2151f3eaa55224b1d0044bb2bdab3573e0
Garrett Olson <garrettdolson@gmail.com>
2014-10-02 18:51:24 -0700

    Refactored nav menu open and closing, events now triggered when cursor gets within 40px of element

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
-var navExpanded,
-    initialLoadPage,
-    currentlySelectedLink;
+var navExpanded = true,
+    nextEventReady = true,
+    initialPageLoad = true;
+    currentlySelectedLink = '';
 
 $(document).ready(function() {
-	navExpanded = true;
-	initialLoadPage = true;
+
+    openCloseNav();
 
     updateDebugInfo();
 
     $('.nav-link').on('click', function() {
-	    	initialLoadPage = false;            
+	    	initialPageLoad = false; 
 
             if($(this).attr('name') !== currentlySelectedLink) {
                 var activeSubNav = $('.subnav-menu.active');
@@ -18,32 +19,63 @@ $(document).ready(function() {
 
             currentlySelectedLink = $(this).attr('name');
 
+            nextEventReady = true;
             updateDebugInfo();
     });
-    $('#nav-menu').on('mouseenter', function() {
-     	if(!navExpanded) {     		
-    		$('#nav-menu').toggleClass('collapsed');
-    		navExpanded = !navExpanded;
-            updateDebugInfo()
+/*   $('#nav-menu').on('mouseenter', function() {
+     	if(!navExpanded) {           	    		
+            $('#nav-menu').find('a').show();
+            $('#nav-menu').find('li span').fadeIn(200);
+            updateDebugInfo();
      	}    	
     });    
-    $('#nav-menu').on('mouseleave', function() {
-     	if(navExpanded && !initialLoadPage) {   	
-    		$('#nav-menu').toggleClass('collapsed');
-    		navExpanded = !navExpanded;   
-            updateDebugInfo()	
+    $('#nav-menu').on('mouseleave', function() {   
+     	if(navExpanded && !initialPageLoad) {             	
+            $('#nav-menu').find('li span').fadeOut(100);
      	}    	
-    });
+    });*/
     $('#nav-menu').on('click', "a[name='treatment']", function() {    
             $('.subnav-menu').addClass('active');
-            updateDebugInfo()    
+            updateDebugInfo();    
     });
 });
 
 function updateDebugInfo() {
+    $('.initial-page-load').html('Initial page load: ' + initialPageLoad.toString().toUpperCase());
     $('.nav-menu-state').html('Nav-Menu Open? ' + navExpanded.toString().toUpperCase());
     $('.currently-selected-link').html('Current Page: ' + currentlySelectedLink);
+    $('.next-event-ready').html('Next event ready: ' + nextEventReady.toString().toUpperCase());
 }
 
-//test
+// Returns proximity to a 'target_element' so that user 
+// actions can be anticipated before reaching actual target
+function openCloseNav() {
+
+    var mX, mY, distanceToTarget,         
+        $distanceToTarget = $('.menu-debugging .distance-from-target'),
+        $element  = $('#nav-menu');
+
+    function calculatedistanceToTarget(elem, mouseX) {
+        return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width())), 2)));
+    }
+
+    $(document).mousemove(function(e) {  
+        mX = e.pageX;
+        distanceToTarget = calculatedistanceToTarget($element, mX);
+
+        // 
+        if(!initialPageLoad && distanceToTarget < 40 && nextEventReady) {
+            nextEventReady = false;
+            navExpanded = !navExpanded; 
+            $('#nav-menu').toggleClass('collapsed');
+            $('#nav-menu').find('li span').fadeOut(100);                   
+        }
+        if(!initialPageLoad && distanceToTarget > 40) {
+            nextEventReady = true;
+        }
+        
+        updateDebugInfo()
+        $distanceToTarget.text("Mouse distanceToTarget from nav menu: " + distanceToTarget);         
+    });    
+};
 
